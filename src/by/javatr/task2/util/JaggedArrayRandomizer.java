61cb59c6bfef5f1c679ca2b5f08758a0e1cee1b9
Vitaly <vmentuz89@gmail.com>
2019-03-04 19:01:17 +0300

    update of logic to stop dependency on initializing

--- src/by/javatr/task2/util/JaggedArrayRandomizer.java
+++ src/by/javatr/task2/util/JaggedArrayRandomizer.java
@@ -2,22 +2,36 @@ package by.javatr.task2.util;
 
 public class JaggedArrayRandomizer {
     private int[][] jaggedArray;
-    public void initializeArray() {
+    private boolean isInitialized = false;
+      
+    /**
+     * @return int[]
+     */
+    public int[][] jaggedArrayGetter() {
+        if (!this.isInitialized) {
+            this.initializedArray();
+        }
+        
+        return this.jaggedArray;
+    }
+    
+    private void initializeArray() {
         this.jaggedArray= new int[5][];
         this.jaggedArray[0] = fullingArray(3);
         this.jaggedArray[1] = fullingArray(7);
         this.jaggedArray[2] = fullingArray(9);
         this.jaggedArray[3] = fullingArray(4);
         this.jaggedArray[4] = fullingArray(5);
+        this.isInitialized = true;
     }
-
-    public int[][] jaggedArrayGetter() {
-
-        return this.jaggedArray;
-    }
-
+    
+    /**
+     * @param int lendth
+     * @return int[]
+     */
     private int[] fullingArray(int length) {
         int[] arrayToFill = new int[length];
+        
         for (int i = 0; i < length; i++) {
             arrayToFill[i] = (int) (Math.random()*100);
         }
