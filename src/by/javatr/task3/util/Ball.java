4499f77fbe9fa80a7803efb831648bee5d19d7a2
Vitaly <vmentuz89@gmail.com>
2019-03-04 18:53:17 +0300

    You don't need that

--- src/by/javatr/task3/util/Ball.java
+++ src/by/javatr/task3/util/Ball.java
@@ -16,38 +16,6 @@ public class Ball implements ItemInterface{
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (null == o) {
-            return false;
-        }
-        if (getClass() != o.getClass()) {
-            return false;
-        }
-        Ball ball = (Ball) o;
-        if (weight != ball.weight) {
-            return false;
-        }
-        if (null == color) {
-            return (color == ball.color);
-        }
-        else {
-            if (!color.equals(ball.color)) {
-                return false;
-            }
-        }
-        return getWeight() == ball.getWeight() &&
-                getColor().equals(ball.getColor());
-    }
-
-    @Override
-    public int hashCode() {
-        return (int)(31*weight + ((null == color) ? 0 : color.hashCode()));
-    }
-
-    @Override
     public int getWeight() {
         return weight;
     }
